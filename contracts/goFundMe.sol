696968252d3e90d7236538fd7260b9445725f2eb
Godand <aezeigwe2003@gmail.com>
2022-08-27 13:32:38 +0000

    goFundMe Complete

--- contracts/goFundMe.sol
+++ contracts/goFundMe.sol
@@ -4,13 +4,15 @@ pragma solidity 0.8.9;
 error invalidState();
 error notFactory();
 error notOwner();
+error failed();
 
 contract goFundMe {
-    
-    address public factory;
+    address immutable factory;
     address public owner;
 
     uint256 public balance;
+    uint256 public amountNeeded;
+    uint256 public totalReceived;
 
     struct funders {
         address funder;
@@ -25,32 +27,80 @@ contract goFundMe {
 
     State public state;
 
-    funders[] private fundersList; 
+    funders[] private fundersList;
 
-    modifier onlyFactory {
-        if (msg.sender == factory)
-        {
+    modifier onlyFactory() {
+        if (msg.sender == factory) {
             _;
-        }else {
+        } else {
             revert notFactory();
         }
     }
 
-    modifier onlyOwner {
-        if (msg.sender == owner)
-        {
+    modifier onlyOwner() {
+        if (msg.sender == owner) {
             _;
-        }else {
+        } else {
             revert notOwner();
         }
     }
 
     modifier inState(State state_) {
-        if (state == state_)
-        {
+        if (state == state_) {
             _;
-        }else {
+        } else {
             revert invalidState();
         }
+    }
+
+    constructor() {
+        factory = msg.sender;
+    }
+
+    receive() external payable inState(State.Funding) 
+    {
+        if ((totalReceived + msg.value) > amountNeeded) {
+            balance += msg.value;
+            totalReceived += msg.value;
+            fundersList.push(funders({funder: msg.sender, amount: msg.value}));
+        } else {
+            revert failed();
+        }
+    }
+
+    function startFunding() external onlyOwner inState(State.StandBy) 
+    {
+        state = State.Funding;
+    }
+
+    function endFunding() external onlyOwner inState(State.Funding) 
+    {
+        state = State.Completed;
+    }
+
+    function pullFunds(uint256 _amount)
+        external
+        onlyOwner
+        inState(State.Completed)
+    {
+        balance -= _amount;
+        (bool sent, ) = payable(owner).call{value: _amount}("");
+        if (!sent) revert failed();
+    }
+
+    function getBalance() external view returns (uint256) 
+    {
+        return balance;
+    }
+
+    function getFunders() external view returns (funders[] memory) 
+    {
+        return fundersList;
+    }
+
+    function initializer(address _owner, uint256 _amount) external onlyFactory 
+    {
+        owner = _owner;
+        amountNeeded = _amount;
     }
 }
